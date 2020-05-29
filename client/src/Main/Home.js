2c7ba604ae3312af9c48ad9449c96f8aecc74cf7
Kikionly <andreeacosma.98@gmail.com>
2020-05-29 14:15:29 +0300

    added client

---
+++ client/src/Main/Home.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+
+function Home() {
+  return (
+    <div>
+    <section className="position-relative pt-12 pt-md-14 mt-n11">
+      <div className="container">
+        <div className="row align-items-center text-center text-md-left">
+          <div className="col-12 col-md-6">
+            <img src="../assets/shopping.png" alt="shopping illustration" className="img-fluid mw-100 float-right mb-6 mb-md-0"/>
+          </div>
+          <div className="col-12 col-md-6">
+            <div className="text-center text-lg-right text-sm-left mb-5 mt-sm-8">
+                <h4 className="tile-info mb-0">
+                Romanian suppliers united together
+                </h4>
+                <h5 className="text-gray-800 large pl-sm-1">
+                  Project Description
+                </h5>
+
+            </div>
+            <p className="mt-4 mb-0 pl-sm-1 text-center text-lg-right text-sm-left">
+              The web-app is meant to connect small, local suppliers who produce fresh ingredients (vegetables, dairy, meat, fruit etc.) and hand-made work with the end user. This comes as an alternative to keep selling products commonly found in local delhi markets in times affected by the current coronavirus.
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </section>
+    <section className="position-relative pt-12 pt-md-14 mt-n11 blue-shape-bg pb-4">
+      <div className="container">
+        <div className="row align-items-center text-center text-md-right mt-5">
+          <div className="col-12 col-md-6 ">
+            <div className="text-center text-lg-left text-sm-left mb-5 mt-sm-8 pt-sm-4 pt-4">
+                <h5 className="text-gray-800 large pl-sm-1">
+                  Project Details
+                </h5>
+
+            </div>
+            <p className="mt-4 mb-0 pl-sm-1 text-center text-lg-left text-sm-left ">
+            The client will be able to contact the supplier and set up a convenient exchange. (Whether it be delivery, pickup or a different pick-up).
+
+            The web-app also serves as an initiative to boost local producers, thus benefiting the economy of the country during these troubled times.
+
+            Functionality can be extended to include big distribution chains which can create bids on different products, thus making sure they have a constant supply of local goods.
+            </p>
+          </div>
+          <div className="col-12 col-md-6">
+            <img src="../assets/people-shopping.png" alt="shopping illustration" className="img-fluid mw-100 float-right mb-6 mb-md-0 pl-sm-1"/>
+          </div>
+
+        </div>
+       
+    </div>
+    
+    </section>
+
+  </div>
+  );
+}
+
+export default Home;
---
+++ client/src/Main/Menu.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {Link, withRouter} from 'react-router-dom'
+import { ReactSVG } from 'react-svg'
+
+
+function Menu() {
+  return (
+    <div>
+        <ul className="navbar  navbar-expand-lg pr-6 pl-6 font-size-sm navbar-light">
+           <a className="navbar-brand" href = '/'>
+               <img src="../assets/logo.svg" width='50%'  alt="Rogrow"/>
+           </a>
+
+           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+               <span className="navbar-toggler-icon"></span>
+          </button>
+           
+           <div className="navbar-collapse collapse navigation-tabs" id="navbarSupportedContent">
+                <ul className="navbar-nav ml-auto pr-3">
+                    <li className="nav-item ">
+                         <Link className="nav-link pl-5" to="/about">About</Link>
+                    </li>
+                    <li className="nav-item ">
+                         <Link className="nav-link pl-5" to="/signin">Signin</Link>
+                    </li>
+                    <li className="nav-item">
+                         <Link className="nav-link pl-5" to="/signup">Signup</Link>
+                    </li>
+                </ul>
+           </div>
+
+        </ul>
+    </div>      
+    
+  );
+}
+
+export default withRouter(Menu);
