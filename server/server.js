4ca7140e0beda84ed8a7f793d2f6d1bed5a32daf
adrian.batrin <adrian.batrin@ligaac.ro>
2020-05-31 19:12:49 +0300

    Environment changes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const dotenv = require('dotenv').config({ path: './config.env' });
 const app = require('./app');
 
+console.log(process.env);
+
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
