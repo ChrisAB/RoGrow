c4a9c78fc39a351b31ec1ac0819dcc42db491123
Kikionly <andreeacosma.98@gmail.com>
2020-05-31 23:02:35 +0300

    moved api related functions to a separate file

---
+++ client/src/auth/index.js
@@ -0,0 +1,39 @@
+import {API} from '../config'
+
+export const registerSeller = async (user) => {
+    console.log(user);
+    return await fetch(`${API}/v1/user`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+export const registerBuyer = async (user) => {
+    console.log(user);
+    return await fetch(`${API}/v1/user`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+
