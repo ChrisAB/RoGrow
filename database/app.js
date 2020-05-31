400cfab97e37d3acfe191c0729561b670a3e0306
IonutMarian27 <48709347+IonutMarian27@users.noreply.github.com>
2020-05-31 14:37:39 +0300

    Add files via upload

---
+++ database/app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const productRoutes = require('./api/routes/products');
+const userRoutes = require('./api/routes/users');
+const cartRoutes = require('./api/routes/carts');
+
+mongoose.connect('mongodb+srv://shop-admin:' + process.env.DB_PW +'@cluster-shop-zdn9f.mongodb.net/test?retryWrites=true&w=majority', 
+    { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'GET, PUT, DELETE, PATCH');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+
+
+// Routes that handle requests
+app.use('/carts', cartRoutes);
+app.use('/products', productRoutes);
+app.use('/users', userRoutes);
+
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
+/*
+app.use((req, resp, next) => {
+    resp.status(200).json({
+        message: 'working boyz'
+    });
+});
+*/
+
+module.exports = app;
+
