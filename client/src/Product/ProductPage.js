7fa8188676ab1110c61c8e2f3018260ef2d8a285
Kikionly <andreeacosma.98@gmail.com>
2020-06-03 15:46:29 +0300

    search added

--- client/src/Product/ProductPage.js
+++ client/src/Product/ProductPage.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom'
-import {getAllProducts} from './productApi'
+import {getAllProducts, getSearchProducts} from './productApi'
 import { isSigned } from "../auth/index";
 import  GeneralProductCard  from "./GeneralProductCard";
 
@@ -8,6 +8,8 @@ import  GeneralProductCard  from "./GeneralProductCard";
 function ProductPage() {
 
     const [products, setProducts] = useState([]);
+    const [productsBySearch, setProductsBySearch] = useState([]);
+
     const [error, setError] = useState(false);
     const {token} = isSigned();
 
@@ -24,13 +26,125 @@ function ProductPage() {
       loadProducts()
     }, [])
 
+    const [values, setValues] = useState({
+      name: undefined,    
+      price: undefined,
+      quantity: undefined,
+      pickupLocation: undefined,
+      origin: undefined,
+      sellerID: undefined,
+      errorVal:""
+    });
+
+  const {
+      name,
+      price,
+      quantity,
+      pickupLocation,
+      origin,
+      sellerID,
+      errorVal
+  } = values;
+
+  const handleChanege = (name) => (event) => {
+    setValues({ ...values, errorVal: false, [name]: event.target.value });
+  };
+
+  const clickSearch = async(e) => {
+    e.preventDefault();
+    const data = await getSearchProducts(token,{
+      name: name,
+      price: price,
+      quantity: quantity,
+      pickupLocation: pickupLocation,
+      origin:origin,
+    })
+          if (data.status !== "success") {
+            setError(data.message);
+          } else {
+            setProducts(data.data);
+    }
+  }
+
+    const searchForm = () => {
+      return (
+                    <div>
+                      <a className="btn btn-lg btn-block blue-bg white-link" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                                Search
+                      </a>
+                      <div class="collapse card p-4" id="collapseExample">
+                          <div  >
+                              <label className="font-weight-bold text-gray-800 pr-1 pl-sm-1 pt-1"> Name: </label>
+                              <input
+                              onChange={handleChanege("name")}
+                              type="text"
+                              className="form-control"
+                              placeholder="Name"
+                              value={name}                              
+                              autoFocus
+                              />
+                          </div>
+                          <div >
+                              <label className="font-weight-bold text-gray-800 pr-1 pl-sm-1 pt-1">Price: </label>
+                              <input
+                              onChange={handleChanege("price")}
+                              type="number"
+                              className="form-control"
+                              placeholder="Price"
+                              value={price}                            
+                             />
+                          </div>
+                          <div >
+                              <label className="font-weight-bold text-gray-800 pr-1 pl-sm-1 pt-1">Available quantity: </label>
+                              <input
+                              onChange={handleChanege("quantity")}
+                              type="number"
+                              className="form-control"
+                              placeholder="Quantity"
+                              value={quantity}                              
+                              />
+                          </div>
+                          <div >
+                              <label className="font-weight-bold text-gray-800 pr-1 pl-sm-1 pt-1">Origin: </label>
+                              <input
+                              onChange={handleChanege("origin")}
+                              type="text"
+                              className="form-control"
+                              placeholder="Origin"
+                              value={origin}                              
+                              />
+                          </div>
+                          <div >
+                              <label className="font-weight-bold text-gray-800 pr-1 pl-sm-1 pt-1">Pick up location: </label>
+                              <input
+                              onChange={handleChanege("pickupLocation")}
+                              type="text"
+                              className="form-control"
+                              placeholder="Pick up location"
+                              value={pickupLocation}                              
+                              />
+                          </div>
+                          <div className="pt-4 pb-2 mt-sm-1  mt-xs-1">
+                              <button
+                              onClick={clickSearch}
+                                className="btn btn-lg btn-block sign-bg"                                
+                              >
+                                Apply search
+                              </button>
+                          </div>
+                          </div>      
+        </div>
+      )
+    }
+
+    
 
     return (
         <section className="position-relative pt-12 pt-md-14 mt-n11">
             
             <div className="mb-5 pb-5 ml-5 mr-5 justify-content-center">
-                <div className="col-12 mt-5 pt-2 card">
-                    search options
+                <div className="col-12 mt-5 pt-2">
+                    {searchForm()}
                 </div>
                 <div className="col-12 row pt-5">
                       {products.map((product, i) => (
--- client/src/Product/productApi.js
+++ client/src/Product/productApi.js
@@ -90,3 +90,19 @@ export const readProduct =  (productId, token) => {
   .catch(err => console.log(err));
 };
 
+
+export const getSearchProducts =  async (token, product) => {
+  return await fetch(`${API}/v1/product?name=${product.name}&price=${product.price}&quantity=${product.quantity}`,{
+      method: "GET",
+      headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
+      }
+  }) .then(response => {
+      return response.json();
+  })
+  .catch(err => console.log(err));
+};
+
+
