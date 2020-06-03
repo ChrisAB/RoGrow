1867aa65e4e047e0ab80d8386e5e7ca290c6eabf
adrian.batrin <adrian.batrin@ligaac.ro>
2020-06-04 00:02:36 +0300

    Fixed a typo in server/app.js related to loading corsOrigin

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ const categories = require('./utils/categories');
 
 const app = express();
 const corsOptions = {
-  origin: `http://${process.env.RUNNINT_AT_IP}:3000`,
+  origin: `http://${process.env.RUNNING_AT_IP}:3000`,
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
